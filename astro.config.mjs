9a6342b11e3595c860289dfe26cb2b2fa4df91ba
Your Name <samdhi2323@gmail.com>
2025-11-02 15:15:44 +0530

    [Fix] Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [svelte()],
   vite: {
     plugins: [tailwindcss()],
