8b2a5506f9edf9071c60a79c642573165bd33c43
Toffikk <slodkitofik@gmail.com>
2025-11-17 01:07:49 +0100

    Revert "chore: enable static for some pages"
    This reverts commit 19f1fceb9524aae0050102ff98616d008cb65599.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
 
 export default defineConfig({
+  output: "server",
   adapter: node({
-      mode: "standalone",
+    mode: "standalone",
   }),
   integrations: [svelte()],
   server: {
