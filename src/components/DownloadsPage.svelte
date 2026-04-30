86f1d2e2ce06c69b621bd16d85a0c2b9bc9b2e0d
Toffikk <slodkitofik@gmail.com>
2026-04-30 23:40:33 +0200

    some jd fixes for exp channels

--- src/components/DownloadsPage.svelte
+++ src/components/DownloadsPage.svelte
@@ -62,7 +62,7 @@
   function handleJavadocRedirect() {
     redirecting = true;
     setTimeout(() => {
-      window.location.href = `/api/v2/jd/?channel=${selectedVersion.replace(/\s*\(.*?\)$/, "")}&experimental=true&project=${project.slug}`;
+      window.location.href = `/api/v2/jd/?channel=${encodeURIComponent(selectedVersion)}&experimental=true&project=${encodeURIComponent(project.slug)}`;
     }, 150);
   }
 
